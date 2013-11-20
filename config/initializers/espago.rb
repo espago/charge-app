d154a26e488792af99e69db1e3a0278e87e3eb79
Jakub Rozmiarek <j.rozmiarek@psp-polska.pl>
2013-11-20 15:40:09 +0100

    espago script settings moved to config file

--- config/initializers/espago.rb
+++ config/initializers/espago.rb
@@ -1,4 +1,6 @@
-Espago.app_id = ENV['APP_ID']
-Espago.app_password = ENV['APP_PASSWORD']
-Espago.public_key = ENV['PUBLIC_KEY']
-Espago.production = false 
+ESPAGO_SETTINGS = YAML.load_file("#{::Rails.root.to_s}/config/espago.yml")[::Rails.env.to_s]
+
+Espago.app_id       = ESPAGO_SETTINGS['app_id']
+Espago.app_password = ESPAGO_SETTINGS['app_password']
+Espago.public_key   = ESPAGO_SETTINGS['public_key']
+Espago.production   = ESPAGO_SETTINGS['production']
